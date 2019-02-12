6cdd707ec7713537ecb6443e51e7a2748237e68f
jordan-laird <jordanmack.laird@gmail.com>
2019-02-12 11:13:51 -0600

    fixing heroku issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import { HomePage } from "./views/HomePage";
-import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { User } from "./unused_components/User";
 import { UserEdit } from "./unused_components/UserEdit";
 import { NavBar } from "./components/NavBar";
@@ -63,11 +63,11 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path="/" render={() => (localStorage.userID ? ( <Redirect to="/home" /> ) : ( <Login />))} />
             {/* <Route path="/logout" component={Logout} /> */}
-            <PrivateRoute path="/companies" render={(props) => <HomePage {...props} currentPrices={this.state.currentPrices} />} />
+            <PrivateRoute path="/home" render={(props) => <HomePage {...props} currentPrices={this.state.currentPrices} />} />
             <PrivateRoute path="/Portfolio" render={(props) => <Portfolio {...props} currentPrices={this.state.currentPrices} /> } />
-            <Route path="/SignUp" component={SignUp} />
+            <Route path="/SignUp" render={() => (localStorage.userID ? (<Redirect to="/home" />) : (<SignUp />))} />
           </Switch>
         </div>
       </BrowserRouter>
